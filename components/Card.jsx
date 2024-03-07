f78d961fc8eae7fad04f80f1cd9b4b26fda1d57c
juanesgv <juanesgarviedma23@gmail.com>
2024-03-07 12:14:38 -0500

    consulta a la api

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,25 +1,41 @@
 import React from 'react'
-import start_fill from '/img/Star_fill.svg'
+import star_fill from '/img/Star_fill.svg'
+import star from '/img/Star.svg'
 
-const Card = () => {
+const Card = ({ value }) => {
+    console.log("lo que estoy enviando", value)
     return (
         <div className='flex flex-col gap-4'>
             <figure className='relative'>
-                <p className=' absolute font-Montserrat text-xs px-2 py-1 text-black-1 bg-yellow-popular rounded-full top-3 left-4'>Popular</p>
-                <img className='rounded max-h-36 w-full object-cover object-center' src='https://images.unsplash.com/photo-1534687941688-651ccaafbff8?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='Imagen'/>
+                {value.isPopular &&
+                    <p className=' absolute font-Montserrat text-xs px-2 py-1 text-black-1 bg-yellow-popular rounded-full top-3 left-4'>Popular</p>
+                }
+                <img className='rounded max-h-36 w-full object-cover object-center' src={value.image} alt='Imagen' />
             </figure>
 
             <div className='flex justify-between'>
-                <p className='text-white font-Montserrat'>House Coffee</p>
-                <p className='font-Montserrat text-xs px-2 py-1 text-black-1 bg-apple-green rounded'>$3.50</p>
+                <p className='text-white font-Montserrat'>{value.name}</p>
+                <p className='font-Montserrat text-xs px-2 py-1 text-black-1 bg-apple-green rounded'>{`$ ${value.pricing}`}</p>
             </div>
             <div className='flex justify-between'>
                 <div className='flex gap-1 items-center justify-between'>
-                    <img src={start_fill} alt='star icon' className='h-5 w-5'/>
-                    <p className='text-white font-Montserrat'>4.85</p>
-                    <p className='font-Montserrat text-text-white opacity-65'>(14 votes)</p>
+                    {value.votes === 0 ? (
+                        <>
+                            <img src={star} alt='star icon' className='h-5 w-5' />
+                            <p className='font-Montserrat text-text-white opacity-65'>No ratings</p>
+                        </>
+                    ) : (
+                        <>
+                            <img src={star_fill} alt='star icon' className='h-5 w-5' />
+                            <p className='text-white font-Montserrat'>{value.rating}</p>
+                            <p className='font-Montserrat text-text-white opacity-65'>{`(${value.votes} votes)`}</p>
+                        </>
+                    )}
+
                 </div>
+                {value.isSoldOut &&
                     <p className='text-red-error font-Montserrat'>Sold Out</p>
+                }
             </div>
         </div>
     )
--- pages/Collections.jsx
+++ pages/Collections.jsx
@@ -1,44 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Tabs from '../components/Tabs';
 import Card from '../components/Card';
+import axios from 'axios';
 
 const Collections = () => {
 
-    const tabData = [
-        {
-          title: 'All products',
-          content: (
-            <div className='grid grid-cols-3 gap-8'>
-              <Card/>
-              <Card/>
-              <Card/>
-              <Card/>
-              <Card/>
-            </div>
-          ),
-        },
-        {
-          title: 'Available now',
-          content: (
-            <div>
-              <p>Contenido de la pestaña 2:</p>
-              <p>Otro párrafo aquí.</p>
-            </div>
-          ),
-        },
-      ];
+  const [coffeList, setCoffeList] = useState([])
 
-    return (
-        <>
-            <div className='flex flex-col gap-4'>
-                <h1 className='font-Montserrat text-white text-3xl font-semibold text-center'>Our Collection</h1>
-                <p className='font-Montserrat text-base text-center text-text-white opacity-75'>Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
-            </div>
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios(import.meta.env.VITE_API_URL)
+        setCoffeList(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  }, [])
 
-            <Tabs tabs = {tabData}/>
-        </>
+  const tabData = [
+    {
+      title: 'All products',
+      content: (
+        <div className='grid grid-cols-3 gap-8'>
+          {coffeList.map((coffee, index) => (
+            <Card
+              key={index}
+              value={coffee}
+            />
+          ))}
+        </div>
+      ),
+    },
+    {
+      title: 'Available now',
+      content: (
+        <div className='grid grid-cols-3 gap-8'>
+          {coffeList.map((coffee, index) => (
+            !coffee.isSoldOut && (
+              <Card
+                key={index}
+                value={coffee}
+              />
+            )
+          ))}
+        </div>
+      ),
+    },
+    {
+      title: 'Popular coffees',
+      content: (
+        <div className='grid grid-cols-3 gap-8'>
+          {coffeList.map((coffee, index) => (
+            coffee.isPopular && (
+              <Card
+                key={index}
+                value={coffee}
+              />
+            )
+          ))}
+        </div>
+      ),
+    },
+  ];
 
-    )
+  return (
+    <>
+      <div className='flex flex-col gap-4'>
+        <h1 className='font-Montserrat text-white text-3xl font-semibold text-center'>Our Collection</h1>
+        <p className='font-Montserrat text-base text-center text-text-white opacity-75'>Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
+      </div>
+
+      <Tabs tabs={tabData} />
+    </>
+
+  )
 }
 
 export default Collections
